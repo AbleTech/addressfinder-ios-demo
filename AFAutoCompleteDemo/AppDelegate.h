d2eca466937059381dcef4f91f4eac73871a36b4
Sean Arnold <me@seanarnold.co.nz>
2015-05-05 15:39:20 +1200

    Initial Commit

---
+++ AFAutoCompleteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFAutoCompleteDemo
+//
+//  Created by Sean Arnold on 5/05/15.
+//  Copyright (c) 2015 Abletech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

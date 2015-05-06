1558c249619d8e970d1d1a9fef86e602b4dcaae6
Sean Arnold <me@seanarnold.co.nz>
2015-05-06 14:33:33 +1200

    Styling Main View Controller - adding logo and text

---
+++ AFAutoCompleteDemo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  AFAutoCompleteDemo
+//
+//  Created by Sean Arnold on 5/05/15.
+//  Copyright (c) 2015 Abletech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ AFAutoCompleteDemo/MainViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainViewController.m
+//  AFAutoCompleteDemo
+//
+//  Created by Sean Arnold on 5/05/15.
+//  Copyright (c) 2015 Abletech. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.afTextField setDelegate:self];
+    [self setNeedsStatusBarAppearanceUpdate];
+    // Do any additional setup after loading the view.
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

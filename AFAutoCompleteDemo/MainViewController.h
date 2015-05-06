b129710f6efa5cc017af642e58ee4a56253ca527
Sean Arnold <me@seanarnold.co.nz>
2015-05-06 16:42:45 +1200

    Adding address fields on address selection

--- AFAutoCompleteDemo/MainViewController.h
+++ AFAutoCompleteDemo/MainViewController.h
@@ -11,4 +11,9 @@
 
 @interface MainViewController : UIViewController <AFAutoCompleteDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet AFAutoComplete *afTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *postalLine1;
+@property (weak, nonatomic) IBOutlet UILabel *postalLine2;
+@property (weak, nonatomic) IBOutlet UILabel *postalLine3;
+@property (weak, nonatomic) IBOutlet UILabel *meshblock;
 @end
--- AFAutoCompleteDemo/MainViewController.m
+++ AFAutoCompleteDemo/MainViewController.m
@@ -40,13 +40,67 @@
 }
 */
 
-- (void)textField:(AFAutoComplete *)textField didEndEditingWithSelection:(NSDictionary *)result {
+- (void)textField:(AFAutoComplete *)textField didEndEditingWithSelection:(NSDictionary *)result
+{
     // Here you are provided with a dictionary that contains:
     // the full address
     // the pxid of the adddress
     
     // An example of future use could be to call the AddressFinder Address Info API to receive address line data,
     // or x & y coordinates etc. https://addressfinder.nz/docs/address_info_api/
+    __block NSDictionary *addressData = @{};
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // sync code
+        addressData = [self getAFAddressInfo:[result objectForKey:@"pxid"]];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Update UI
+            self.postalLine1.text = [addressData objectForKey:@"postal_line_1"];
+            self.postalLine2.text = [addressData objectForKey:@"postal_line_2"];
+            self.postalLine3.text = [addressData objectForKey:@"postal_line_3"];
+            self.meshblock.text = [addressData objectForKey:@"meshblock"];
+            
+            self.postalLine1.hidden = FALSE;
+            self.postalLine2.hidden = FALSE;
+            self.postalLine3.hidden = FALSE;
+            self.meshblock.hidden = FALSE;
+        });
+    });
+
     
+}
+
+- (NSDictionary *)getAFAddressInfo:(NSString *)pxid
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:
+                      @"AddressFinder" ofType:@"plist"];
+    NSMutableDictionary *plistDict = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    NSString *afKey = [plistDict objectForKey:@"addressFinderKey"];
+    NSString *afSecret = [plistDict objectForKey:@"addressFinderSecret"];
+    
+    NSDictionary* data = @{};
+    
+    // Background work
+    NSString *unEncodedUrl = [NSString stringWithFormat:@"%@%@&key=%@&secret=%@", @"https://api.addressfinder.nz/api/address/info?format=json&pxid=", pxid, afKey, afSecret];
+    NSString* url = [unEncodedUrl stringByAddingPercentEscapesUsingEncoding:
+                     NSUTF8StringEncoding];
+    // Send a synchronous request
+    NSURLRequest * urlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSURLResponse * response = nil;
+    NSError * error = nil;
+    NSLog(@"<AFAutoComplete calling API %@", url);
+    NSData * responseData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                                  returningResponse:&response
+                                                              error:&error];
+    
+    NSMutableArray *results = [[NSMutableArray alloc] initWithArray:@[]];
+    if (error == nil)
+    {
+        NSError *parseError = nil;
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&parseError];
+        NSLog(@"Results were %@", dictionary);
+            data = dictionary;
+    }
+
+    return data;
 }
 @end
